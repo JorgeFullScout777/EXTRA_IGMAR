16d560811ee4102e4984956d5ccdc781796f7d2b
JorgeFullScout777 <jorgefullscout@gmail.com>
2024-04-23 12:53:21 -0600

    Seeders

---
+++ database/migrations/2024_04_23_183047_create_approbations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+     // Esta tabla es para ver si un usuario esta aprobado para particiar en una comunidad, si no lo esta, no puede hacer nada
+    public function up()
+    {
+        Schema::create('approbations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('community_id')->constrained('communities')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('approbations');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,9 +12,49 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Jorge Luis',
-            'email' => 'jorgefullscout2.0@gmail.com',
-            'password' => Hash::make('password'),
+            ['name' => 'JorgeFullScout',  'email' => 'jorgefullscout@gmail.com',  'password' => Hash::make('password')],
+            ['name' => 'Maria Fernanda',  'email' => 'maria.fernanda@gmail.com',  'password' => Hash::make('password')],
+        ]);
+
+        DB::table('communities')->insert([
+            ['name' => 'Laravel Community',  'description' => 'The best PHP framework',  'user_id' => 1],
+            ['name' => 'Vue Community',  'description' => 'The best JS framework',  'user_id' => 2],
+        ]);
+
+        DB::table('channels')->insert([
+            ['name' => 'Laravel Channel 1',  'description' => 'Descripcion del canal 1',  'community_id' => 1, 'user_id' => 1],
+            ['name' => 'Laravel Channel 2',  'description' => 'Descripcion del canal 2',  'community_id' => 1, 'user_id' => 1],
+
+            ['name' => 'Vue Channel 1',  'description' => 'Descripcion del canal 1',  'community_id' => 2, 'user_id' => 2],
+            ['name' => 'Vue Channel 2',  'description' => 'Descripcion del canal 2',  'community_id' => 2, 'user_id' => 2],
+        ]);
+
+        DB::table('posts')->insert([
+            ['title' => 'Post 1 comunidad laravel',  'content' => 'Contenido del post 1',  'community_id' => 1, 'user_id' => 1],
+            ['title' => 'Post 2 comunidad laravel',  'content' => 'Contenido del post 2',  'community_id' => 1, 'user_id' => 1],
+
+            ['title' => 'Post 1 comunidad vue',  'content' => 'Contenido del post 1',  'community_id' => 2, 'user_id' => 2],
+            ['title' => 'Post 2 comunidad vue',  'content' => 'Contenido del post 2',  'community_id' => 2, 'user_id' => 2],
+        ]);
+
+        DB::table('comments')->insert([
+            ['content' => 'Contenido del comentario 1 comunidad laravel',  'post_id' => 1, 'user_id' => 1],
+            ['content' => 'Contenido del comentario 2 comunidad laravel',  'post_id' => 1, 'user_id' => 1],
+
+            ['content' => 'Contenido del comentario 1 comunidad vue',  'post_id' => 2, 'user_id' => 2],
+            ['content' => 'Contenido del comentario 2 comunidad vue',  'post_id' => 2, 'user_id' => 2],
+        ]);
+
+        DB::table('messages')->insert([
+            ['content' => 'Contenido del mensaje 1',  'user_id' => 1, 'channel_id' => 1],
+            ['content' => 'Contenido del mensaje 2',  'user_id' => 2, 'channel_id' => 2],
+
+            ['content' => 'Contenido del mensaje 1',  'user_id' => 2, 'channel_id' => 3],
+            ['content' => 'Contenido del mensaje 2',  'user_id' => 1, 'channel_id' => 4],
+        ]);
+
+        DB::table('approbations')->insert([
+            ['user_id' => 1,  'community_id' => 2],
         ]);
     }
 }
