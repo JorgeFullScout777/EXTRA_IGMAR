1293b9716b88a0d478bfbc58392c435fbee22778
Erick Hdz <139566870+Zanobax@users.noreply.github.com>
2024-04-25 05:36:56 -0600

    fecha

--- resources/js/Pages/Post.vue
+++ resources/js/Pages/Post.vue
@@ -28,10 +28,10 @@ import { Head, router } from '@inertiajs/vue3';
                                 <p class="text-sm text-gray-500">  {{ comment.username }} </p>
                                 <h5>  {{ comment.content }} </h5>
                                 <h6 class=" items-center text-xs font-normal text-gray-500 hover:underline dark:text-gray-400">
-                                    Creado:{{ comment.created_at }}
+                                    Creado:{{ formaterDate(comment.created_at)}}
                                 </h6>
                                 <h6 class=" items-center text-xs font-normal text-gray-500 hover:underline dark:text-gray-400">
-                                    Editado:{{ comment.updated_at }}</h6>
+                                    Editado:{{ formaterDate(comment.updated_at) }}</h6>
 
                                 <div>
                                     <button type="button" @click="editcomment(comment.id,)" v-if="comment.user_id==$page.props.auth.user.id" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">Editar</button>
@@ -154,6 +154,8 @@ export default {
             },
             allowOutsideClick: () => !Swal.isLoading()
         });
+    },formaterDate(date){
+        return new Date(date).toLocaleString();
     }
     },
     mounted() {
